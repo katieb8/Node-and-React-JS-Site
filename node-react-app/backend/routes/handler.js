79b6d525c30182879fca0b886cf8469712c1dcc8
Katie Burdett <Katie.Burdett@ao.com>
2024-01-26 16:23:47 +0000

    chore: add routing handler

---
+++ node-react-app/backend/routes/handler.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/tweets', (req, res) => {
+    const str = [{
+        "name": "katie",
+        "msg": "this is my first tweet",
+        "username": "KB"
+    }];
+    res.end(JSON.stringify(str));
+});
+
+router.post('/addTweet', (req, res) => {
+    res.end('not implemented yet');
+});
+
+module.exports = router;
